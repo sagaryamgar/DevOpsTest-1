f10b6ef1255fe9f13d0e6a81fc1975ba58474685
sagar <yamgar.sagar@yahoo.com>
2024-07-23 07:01:04 +0530

    Update DevOpsTest1ApplicationTests.java
    Initial commit

--- src/test/java/com/test/devops/DevOpsTest1ApplicationTests.java
+++ src/test/java/com/test/devops/DevOpsTest1ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.test.devops;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.test.devops.entity.LogEntityStore;
+import com.test.devops.intrf.LogRepository;
+import com.test.devops.intrf.ReadFile;
 
 @SpringBootTest
 class DevOpsTest1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Value(value = "${file.path}")
+	String filePath;
 
+	@Autowired
+	LogRepository logRepository;
+
+	@Autowired
+	ReadFile readFile;
+
+	@Test
+	void readFile() { 
+		readFile.readFile(filePath);
+	};
+
+	@Test
+	public void whenFindingLogEntityById_thenCorrect() {
+		logRepository.save(new LogEntityStore( "stgra",  "APPLICATION_LOG",  "12345", 1491377495210L,  true));
+		assert(logRepository.findById("stgra").isPresent());
+	}
 }
